09747ba9c466aa900f9df976e5ffc3dcc7793082
rafael-cardoso-py <felipe.xxk7@gmail.com>
2026-03-03 11:53:50 -0300

    rafael-cardoso
    GitHub
    codigo de hamburgueria

---
+++ Projeto_app_Rafael Cardoso.py
@@ -0,0 +1,19 @@
+print('n\Hamburgueria\n')
+# print('1.Cadastro') # cadastro e login
+print('1. Cardápio')
+print('2. Delivery')
+print('0. Sair')
+
+while True:
+    escolha_menu = input('Escolha uma opção: ')
+    if escolha_menu == '1':
+        print('carregando cardápio...')
+        lanches_disponiveis = input('\nSelecione seu lanche\n 1. Combo Carne\n 2. Combo coca\n 3. combo suco\n \nEscolha uma opção:\n')
+        # print('\n1. combo carne\n')
+        Selecione_os_acompanhamentos = input('\nescolha até 2 acompanhamentos\n 1. nuggets \n 2. Batata extra\n 3. molho barbecue\n 4. Não quero acompanhamento     
+        # Deve selecionar os lanches e acompanhamentos.
+        Selecione_os_acompanhamentos = input('\nEscolha o segundo acompanhamento\n 1. nuggets\n 2. Batata extra\n 3. molho barbecue\n 4. Não quero acompanhamento
+    print('\n Finalizando Pedido...\n')
+    if escolha_menu == '2' input('\nDigite seu Endereço\\n')
+
+    
